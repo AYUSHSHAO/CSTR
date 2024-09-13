3f4c26d9e9871f406883ca93d81f53b7511c2aac
AYUSHSHAO <ayushsharma2019iit@gmail.com>
2024-09-13 17:24:34 +0530

    added code in graph_compare.py

--- TD3/With Noise/HIRO/graph_compare.py
+++ TD3/With Noise/HIRO/graph_compare.py
@@ -2,21 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-tot_time = 15*24*60
-dt = 60
+tot_time = 4
+dt = 0.05
 # Load the CSV file into a DataFrame
-df_TD3 = pd.read_csv('./TD3/Protein_TD3_reward.csv', header=None)
-df_HIRO = pd.read_csv('./HIRO/Protein_HIRO_reward.csv', header=None)
+df_TD3 = pd.read_csv('/Users/ayushsharma/One_Drive_IITD/Design_Project/Codes/CSTR/TD3/With Noise/HIRO/CSTR_HIRO.csv', header=None)
+df_HIRO_TD3 = pd.read_csv('/Users/ayushsharma/One_Drive_IITD/Design_Project/Codes/CSTR/TD3/With Noise/HIRO/CSTR_HIRO.csv', header=None)
+df_HIRO_PPO = pd.read_csv('/Users/ayushsharma/One_Drive_IITD/Design_Project/Codes/CSTR/PPO/With Noise/HIRO/HIRO/CSTR_HIRO.csv',header=None)
 
-row_index_TD3 =30  # For example, to get the second row (index starts from 0)
+row_index_TD3 =1  # For example, to get the second row we equate to 1 (index starts from 0)
 specific_row_TD3 = df_TD3.iloc[row_index_TD3]
 
 print(specific_row_TD3)
 
-row_index_HIRO = 48  # For example, to get the second row (index starts from 0)
-specific_row_HIRO = df_HIRO.iloc[row_index_HIRO]
+row_index_HIRO = 2  # For example, to get the second row we equate to 1 (index starts from 0)
+specific_row_HIRO_TD3 = df_HIRO_TD3.iloc[row_index_HIRO]
 
-print(specific_row_HIRO)
+print(specific_row_HIRO_TD3)
+
+row_index_HIRO = 2  # For example, to get the second row we equate to 1 (index starts from 0)
+specific_row_HIRO_PPO = df_HIRO_TD3.iloc[row_index_HIRO]
+
+print(specific_row_HIRO_PPO)
+
+
 
 plt.figure()
 time = np.linspace(0, tot_time, int(tot_time / dt))
@@ -24,14 +32,17 @@ time = np.linspace(0, tot_time, int(tot_time / dt))
 plt.plot(time,specific_row_TD3, label='TD3')
 
 # Plot the second data series
-plt.plot(time,specific_row_HIRO, label='HIRO')
+plt.plot(time,specific_row_HIRO_TD3, label='HIRO TD3')
 
-T1 = 590  # target
+# Plot the third data series
+plt.plot(time,specific_row_HIRO_PPO, label='HIRO PPO')
+
+T1 = 0.143  # target
 ta = np.ones(int(tot_time / dt)) * T1
 plt.plot(time, ta, color='tab:orange', linewidth=2, label='reference concentration')
-plt.title('Values from Two Different CSV Files')
-plt.xlabel('time (min)')
-plt.ylabel('Protein')
+plt.title('Comparison')
+plt.xlabel('time (Hours)')
+plt.ylabel('Propylene Glycol')
 plt.legend()
 plt.savefig('Comparision.png', bbox_inches = 'tight')
 plt.close()
